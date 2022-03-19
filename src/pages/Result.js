923da10be989a5b6da0ac4d9a74b0ea2b4c3a0ce
lor-rainemanalo <lorrainemanalo1026@gmail.com>
2022-03-19 19:07:00 +0800

    add home nav to result

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function Result() {
   const navigate = useNavigate();
 
   return (
     <div>
+      <Header />
       {/* <img alt="Result" /> */}
       <h2>you got</h2>
       <h1>score here</h1>
