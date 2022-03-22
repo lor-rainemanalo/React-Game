4577e2096170b276d0755ecc0a75f6dfe89e2abd
lor-rainemanalo <lorrainemanalo1026@gmail.com>
2022-03-22 18:24:06 +0800

    add game music, toggle on or off

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sound from "react-sound";
 
 import Home from "./pages/Home";
 import GameProper from "./pages/GameProper";
@@ -9,10 +10,21 @@ import GeneralTrivia from "./components/general/GeneralTrivia";
 import ScienceTrivia from "./components/science/ScienceTrivia";
 import HistoryTrivia from "./components/history/HistoryTrivia";
 import TechTrivia from "./components/technology/TechTrivia";
+import RussianRoulette from "./mp3/Russian Roulette Instrumentals.mp3";
+import on from "./components/images/on.png";
+import off from "./components/images/off.png";
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <Router>
+      <Sound
+        url={RussianRoulette}
+        playStatus={isPlaying ? "PLAYING" : "PAUSED"}
+        volume={10}
+        loop={true}
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/spin" element={<GameProper />} />
@@ -22,6 +34,13 @@ function App() {
         <Route path="/technology" element={<TechTrivia />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
+      <div onClick={() => setIsPlaying(!isPlaying)}>
+        {!isPlaying ? (
+          <img className="audio-img" src={off} alt="Muted" />
+        ) : (
+          <img className="audio-img" src={on} alt="Unmuted" />
+        )}
+      </div>
     </Router>
   );
 }
